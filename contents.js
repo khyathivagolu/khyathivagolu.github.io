2bf6cee8fc9f8bf3ba59ac63874058ebcb8e4d57
khyathivagolu <khyathi@iitk.ac.in>
2020-04-03 11:35:12 +0530

    added a headerand footer to the page

--- contents.js
+++ contents.js
@@ -1,3 +1,12 @@
 "use strict";
 console.log("helloHELLOOOOOOOOO");
 var i1=4;
+function dropdownMenu() {
+    var x= document.getElementById("dropdownIcon");
+    if (x.className==="navbar"){
+        x.className+=" mobile";
+    }
+    else {
+        x.className="navbar";
+    }
+}
